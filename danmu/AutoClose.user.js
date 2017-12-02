cea395609094feadec721dddd7c17a3aa82306e3
Klee <34185826+KleeU@users.noreply.github.com>
2017-12-02 22:44:57 +0800

    Add files via upload

---
+++ danmu/AutoClose.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         auto close bullet screen 
+// @version      1.0
+// @description  2333
+// @author       Klee
+// @match        *://*.bilibili.com/*
+// @grant        none
+// @run-at       @run-at document-idle
+// @updateURL https://github.com/KleeU/userscript/raw/master/danmu/AutoClose.user.js
+// @downloadURL https://github.com/KleeU/userscript/raw/master/danmu/AutoClose.user.js
+
+// ==/UserScript==
+
+
+(function() {
+
+    clean(2000);
+    function clean(seconds){
+        setTimeout(function(){
+            $("i[name='ctlbar_danmuku_close']").click();
+        },seconds);
+    }
+})();
